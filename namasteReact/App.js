8596d5b74480ad01c51437aa8a79ced8b21d81b8
vvennila91 <ypriyapriyanka1@gmail.com>
2024-02-02 11:54:03 +0530

    initial commit

---
+++ namasteReact/App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id:"heading"},"My World is Murugar")
+        const  root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(heading)
